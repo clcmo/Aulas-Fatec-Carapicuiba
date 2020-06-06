60ea16b0f90787215822838ea3b2f35841e47a97
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-06-06 13:46:26 -0300

    Fibonacci

--- 3 semestre/ed_mac/Aula 08/src/TestesRecursao.java
+++ 3 semestre/ed_mac/Aula 08/src/TestesRecursao.java
@@ -5,5 +5,12 @@ class TestesRecursao{
 		return n * fatorial(n -1);
 	}
 
+	static int fibonacci(int n){
+		if(n == 1 || n == 2)
+			return 1;
+		return fiboacci(n-2) + fibonacci(n-1);
+	}
 
+	System.out.println("Fatorial de 4 é " + fatorial(4));
+	System.out.println("Fibonacci de 4 é " + fibonacci(4));
 }
